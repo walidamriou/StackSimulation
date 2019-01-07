8164fa6a000e418ec73bde3c8b2efc7862f3cab7
Walid Amriou <walidamriou@gmail.com>
2019-01-07 21:46:49 +0100

    Update pile.java

--- code/pile.java
+++ code/pile.java
@@ -1,44 +1,42 @@
 package walidamriou;
 
 public class pile {
-    private int Sommet;
-    private int i;
+    private int Sommet=0;
     private int[] element;
 
-    //initialisation de la pile
-    public pile(){
-        Sommet = 0;
-        i=0;
+    pile(){
+
     }
 
+    //initialisation de la pile
     public pile(int max){
         element = new int[max];
-        Sommet = max;
+        //Sommet = max;
 
     }
 
     //La pile est-elle vide?
 
     public boolean pileVide(){
-        return (i == 0); //if i==0 so the list is vide and return true if is vode
+        return (Sommet == 0); //if i==0 so the list is vide and return true if is vode
     }
 
     //Insertion dans la pile
 
     public void empiler(int v){
-        if (i == Sommet){
-            System.out.println("Le pile est in max. Essayez de le vider d'abord.\n\n");
+        if (Sommet+1 == element.length){
+            erreur("Le pile est in max. Essayez de le vider d'abord.\n\n");
         }
 
         else {
-            i++;
-            element[i]=v;
+            element[Sommet]=v;
             System.out.println("Le numero a ete charge avec succès.\n" +
                     "La valure: " +
                     v +
                     " est sur la postion " +
-                    i +
+                    Sommet +
                     " .\n");
+            Sommet++;
         }
 
     }
@@ -47,34 +45,33 @@ public class pile {
 
     public int depiler(){
         int depiler_var=0;
-        if (i == 0) {
-            System.out.println("Impossible de decharger, car La pile est vide.\n\n");
+        if (Sommet == 0) {
+            erreur("Impossible de decharger, car La pile est vide.\n\n");
         }
         else {
-            depiler_var = element[i];
-            element[i]= 0;
+            Sommet--;
+            depiler_var = element[Sommet];
+            element[Sommet]= 0;
             System.out.println("Le numero a ete decharge avec succès.\n" +
                     "La valure: " +
                     depiler_var +
                     " est a partir la postion " +
-                    i +
+                    Sommet +
                     " .\n");
-            i--;
-
         }
         return depiler_var;
     }
 
     //Vidage de la pile
     public void ViderPile(){
-        if (i==0){
-            System.out.println("La pile est vide.\n");
+        if (Sommet==0){
+            erreur("La pile est vide.\n");
         }
         else {
-            for (int i2=0;i2<i;i2++){
+            for (int i2=0;i2<Sommet;i2++){
                 element[i2]=0;
             }
-            i=0;
+            Sommet=0;
             System.out.println("la pile ete decharge avec succes.\n" +
                     "La pile est vide.\n");
         }
@@ -82,13 +79,13 @@ public class pile {
     }
 
     public void ListPile(){
-        if (i==0){
-            System.out.println("La pile est vide.\n");
+        if (Sommet==0){
+            erreur("La pile est vide.\n");
         }
         else {
             System.out.println("la pile contient: \n");
-            int b=i+1;
-            for (int i2=1;i2<b;i2++){
+            int b=Sommet;
+            for (int i2=0;i2<b;i2++){
                 System.out.println("- La valure: " +
                         element[i2] +
                         " Sur la position " +
@@ -102,8 +99,7 @@ public class pile {
     }
 
     private void erreur(String mes){
-
+        System.out.println(mes);
     }
-
 
 }
